8b9e2cbe6037b76539b0cd83770ecc51e84f0a2d
yomunsam <yomunsam@outlook.com>
2019-12-10 13:33:55 +0800

    meow~~

---
+++ README.md
@@ -0,0 +1,3 @@
+# TinaX Framework - Virtual File System.
+
+TinaX VFS 是 TinaX Framework 中的资源管理模块。现在我们把它拆出来，作为独立的包存在。
---
+++ Runtime/Scripts/VFSKit.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TinaX.VFSKit
+{
+    public class VFSKit : IVFS
+    {
+        public T LoadAsset<T>(string assetPath) where T : UnityEngine.Object
+        {
+            throw new NotImplementedException();
+        }
+
+        public UnityEngine.Object LoadAsset(string assetPath, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> LoadAssetAsync<T>(string assetPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UnityEngine.Object> LoadAssetAsync(string assetPath, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadAssetAsync<T>(string assetPath, Action<T> callback) where T : UnityEngine.Object
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadAssetAsync(string assetPath, Type type, Action<UnityEngine.Object> callback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
