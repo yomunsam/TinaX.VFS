e908af96d15b78aa109a24f9409b34e2aa796515
yomunsam <yomunsam@outlook.com>
2020-04-15 18:31:50 +0800

    喵呜

---
+++ Runtime/Scripts/VFSFacade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatLib;
+
+namespace TinaX.VFSKit
+{
+    public class VFS : Facade<IVFS>
+    {
+    }
+}
