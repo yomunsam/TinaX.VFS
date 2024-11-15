c7ac919aaee52210d1266e373751bccfa7d5c458
yomunchan <i@yomu.moe>
2024-11-15 22:47:46 +0800

    temp

--- Runtime/Scripts/Services/IVFS.cs
+++ Runtime/Scripts/Services/IVFS.cs
@@ -5,58 +5,59 @@ using TinaX.Exceptions;
 using TinaX.VFS.Querier;
 using UObject = UnityEngine.Object;
 
+#nullable enable
 namespace TinaX.VFS
 {
     public interface IVFS
     {
-        UniTask<IAsset> LoadAssetAsync(string loadPath, Type type, string variant = null);
-        AssetQueryResult QueryAsset(string loadPath, string variant = null);
+        UniTask<IAsset?> LoadAssetAsync(string loadPath, Type type, string? variant = null);
+        AssetQueryResult QueryAsset(string loadPath, string? variant = null);
 
-        //#region 同步加载系列
+        #region 同步加载系列
 
-        //UObject Load(string assetPath, Type type);
-        //T Load<T>(string assetPath) where T : UObject;
+        UObject? Load(string assetPath, Type type);
+        T? Load<T>(string assetPath) where T : UObject;
 
-        //IAsset LoadAsset(string assetPath, Type type);
-        //IAsset LoadAsset<T>(string assetPath) where T : UObject;
+        IAsset? LoadAsset(string assetPath, Type type);
+        IAsset? LoadAsset<T>(string assetPath) where T : UObject;
 
-        //#endregion
+        #endregion
 
-        //#region 异步加载 callback系列
-        //void LoadAsync(string assetPath, Type type, Action<UObject, XException> callback);
-        //void LoadAsync(string assetPath, Type type, Action<UObject> callback);
-        //void LoadAsync<T>(string assetPath, Action<T, XException> callback) where T : UObject;
-        //void LoadAsync<T>(string assetPath, Action<T> callback) where T : UObject;
+        #region 异步加载 callback系列
+        void LoadAsync(string assetPath, Type type, Action<UObject?, XException> callback);
+        //void LoadAsync(string assetPath, Type type, Action<UObject?> callback);
+        //void LoadAsync<T>(string assetPath, Action<T?, XException> callback) where T : UObject;
+        //void LoadAsync<T>(string assetPath, Action<T?> callback) where T : UObject;
 
-        //void LoadAssetAsync(string assetPath, Type type, Action<IAsset, XException> callback);
-        //void LoadAssetAsync(string assetPath, Type type, Action<IAsset> callback);
-        //void LoadAssetAsync<T>(string assetPath, Action<IAsset, XException> callback);
-        //void LoadAssetAsync<T>(string assetPath, Action<IAsset> callback);
-        //#endregion
+        //void LoadAssetAsync(string assetPath, Type type, Action<IAsset?, XException> callback);
+        //void LoadAssetAsync(string assetPath, Type type, Action<IAsset?> callback);
+        //void LoadAssetAsync<T>(string assetPath, Action<IAsset?, XException> callback);
+        //void LoadAssetAsync<T>(string assetPath, Action<IAsset?> callback);
+        #endregion
 
-        //#region 异步加载 async/await系列
-        //UniTask<T> LoadAsync<T>(string assetPath, CancellationToken cancellationToken = default) where T : UObject;
-        //UniTask<UObject> LoadAsync(string assetPath, Type type, CancellationToken cancellationToken = default);
+        #region 异步加载 async/await系列
+        UniTask<T?> LoadAsync<T>(string assetPath, CancellationToken cancellationToken = default) where T : UObject;
+        UniTask<UObject?> LoadAsync(string assetPath, Type type, CancellationToken cancellationToken = default);
 
-        //UniTask<IAsset> LoadAssetAsync(string assetPath, Type type, CancellationToken cancellationToken = default);
+        UniTask<IAsset?> LoadAssetAsync(string assetPath, Type type, CancellationToken cancellationToken = default);
 
-        ///// <summary>
-        ///// 异步加载资产
-        ///// </summary>
-        ///// <typeparam name="TAsset">资产类型</typeparam>
-        ///// <param name="assetPath">加载路径</param>
-        ///// <param name="variant">变体</param>
-        ///// <param name="cancellationToken"></param>
-        ///// <returns></returns>
-        //UniTask<IAsset<TAsset>> LoadAssetAsync<TAsset>(string assetPath, string variant = null, CancellationToken cancellationToken = default) where TAsset : UObject;
-        //#endregion
+        /// <summary>
+        /// 异步加载资产
+        /// </summary>
+        /// <typeparam name="TAsset">资产类型</typeparam>
+        /// <param name="assetPath">加载路径</param>
+        /// <param name="variant">变体</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        UniTask<IAsset<TAsset>?> LoadAssetAsync<TAsset>(string assetPath, string? variant = null, CancellationToken cancellationToken = default) where TAsset : UObject;
+        #endregion
 
-        //#region 加载Scene
-        //UniTask<ISceneAsset> LoadSceneAsync(string scenePath);
-        //void LoadSceneAsync(string scenePath, Action<ISceneAsset, XException> callback);
+        #region 加载Scene
+        UniTask<ISceneAsset?> LoadSceneAsync(string scenePath);
+        void LoadSceneAsync(string scenePath, Action<ISceneAsset, XException> callback);
 
-        //ISceneAsset LoadScene(string scenePath);
-        //#endregion
+        ISceneAsset LoadScene(string scenePath);
+        #endregion
 
         void Release(UObject asset);
         
--- Runtime/Scripts/Services/VFSService.cs
+++ Runtime/Scripts/Services/VFSService.cs
@@ -7,6 +7,8 @@ using TinaX.Core.Helper.Platform;
 using TinaX.Core.Utils;
 using TinaX.Exceptions;
 using TinaX.Options;
+using TinaX.Services;
+using TinaX.Services.Builtin.Base;
 using TinaX.Systems.Pipeline;
 using TinaX.VFS.Assets;
 using TinaX.VFS.Internal;
@@ -27,7 +29,7 @@ using UObject = UnityEngine.Object;
 #nullable enable
 namespace TinaX.VFS.Services
 {
-    public class VFSService : IVFS, IVFSInternal
+    public class VFSService : IVFS, IVFSInternal, IAssetService
     {
         //------------固定字段--------------------------------------------------------------------------
         private readonly VFSOptions m_Option;
@@ -46,6 +48,7 @@ namespace TinaX.VFS.Services
             this.m_Services = services;
 
             m_LoadAssetAsyncPipeline = LoadAssetPipelineDefault.CreateAsyncDefault();
+            m_LoadAssetPipeline = LoadAssetPipelineDefault.CreateDefault();
 
             m_Hans = LocalizationUtil.IsHans();
             m_AssetManager = new VFSAssetManager();
@@ -70,12 +73,116 @@ namespace TinaX.VFS.Services
         private AssetQuerier? m_AssetQuerier;
 
         private XPipeline<ILoadAssetAsyncHandler> m_LoadAssetAsyncPipeline;
+        private XPipeline<ILoadAssetHandler> m_LoadAssetPipeline;
+
+        string IBuiltinServiceBase.ImplementerName => "TinaX.VFS";
+
         //------------公开方法（服务接口对外）--------------------------------------------------------------------------
 
-        public async UniTask<IAsset> LoadAssetAsync(string loadPath, Type type, string? variant = null)
+        public async UniTask<IAsset?> LoadAssetAsync(string loadPath, Type type, string? variant = null)
         {
             var asset = await DoLoadAssetAsync(loadPath, type, variant);
             return asset;
+        }
+
+        public IAsset? LoadAsset(string assetPath, Type type)
+        {
+            var asset = DoLoadAsset(assetPath, type);
+            return asset;
+        }
+
+        public UObject? Load(string assetPath, Type type)
+        {
+            return LoadAsset(assetPath, type)?.Asset;
+        }
+
+        public IAsset? LoadAsset<T>(string assetPath) where T : UObject
+        {
+            return LoadAsset(assetPath, typeof(T));
+        }
+
+        public T? Load<T>(string assetPath) where T : UObject
+        {
+            return LoadAsset<T>(assetPath)?.Get<T>();
+        }
+
+
+
+        public void LoadAsync(string assetPath, Type type, Action<UObject, XException> callback)
+        {
+
+        }
+
+        //public void LoadAsync(string assetPath, Type type, Action<UObject> callback)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void LoadAsync<T>(string assetPath, Action<T, XException> callback) where T : UObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void LoadAsync<T>(string assetPath, Action<T> callback) where T : UObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void LoadAssetAsync(string assetPath, Type type, Action<IAsset, XException> callback)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void LoadAssetAsync(string assetPath, Type type, Action<IAsset> callback)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void LoadAssetAsync<T>(string assetPath, Action<IAsset, XException> callback)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void LoadAssetAsync<T>(string assetPath, Action<IAsset> callback)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public UniTask<T?> LoadAsync<T>(string assetPath, CancellationToken cancellationToken = default) where T : UObject
+        {
+            throw new NotImplementedException();
+        }
+
+        public async UniTask<UObject?> LoadAsync(string assetPath, Type type, CancellationToken cancellationToken = default)
+        {
+            var asset = await this.LoadAssetAsync(assetPath, type, cancellationToken);
+            return asset?.Asset;
+        }
+
+        public async UniTask<IAsset?> LoadAssetAsync(string assetPath, Type type, CancellationToken cancellationToken = default)
+        {
+            var asset = await DoLoadAssetAsync(assetPath, type, cancellationToken: cancellationToken);
+            return asset;
+        }
+
+        public UniTask<IAsset<TAsset>?> LoadAssetAsync<TAsset>(string assetPath, string? variant = null, CancellationToken cancellationToken = default) where TAsset : UObject
+        {
+            throw new NotImplementedException();
+        }
+
+        public UniTask<ISceneAsset?> LoadSceneAsync(string scenePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadSceneAsync(string scenePath, Action<ISceneAsset, XException> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISceneAsset LoadScene(string scenePath)
+        {
+            throw new NotImplementedException();
         }
 
 
@@ -178,5 +285,35 @@ namespace TinaX.VFS.Services
             return payload.LoadedAsset!;
         }
 
+        /// <summary>
+        /// 加载资产的内部方法
+        /// </summary>
+        /// <param name="loadPath"></param>
+        /// <param name="type"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        private VFSAsset DoLoadAsset(string loadPath, Type type, string? variant = null)
+        {
+            //context
+            var context = new LoadAssetContext(m_AssetQuerier!, m_AssetManager)
+            {
+                HansLog = m_Hans,
+            };
+            //payload
+            var payload = new LoadAssetPayload(loadPath, variant ?? m_ConfigModel!.GlobalAssetConfig.DefaultAssetBundleVariant);
+
+            m_LoadAssetPipeline.Start(handler =>
+            {
+                handler.LoadAsset(ref context, ref payload);
+                return !context.BreakPipeline;
+            });
+
+            if (payload.LoadedAsset == null)
+                throw new NotFoundException($"Load asset failed, not found : {loadPath}", loadPath);
+
+            return payload.LoadedAsset!;
+        }
+
+        
     }
 }
